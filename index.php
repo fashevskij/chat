1750526c0613feda280215bf7a3b40c78071b1c0
Fashevskij <fashevskij@gmail.com>
2020-10-09 09:49:11 +0300

    #1 - add start page, and button autorization

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+		  
+<div id="up">
+
+		<div id="logo">
+			 <img src="/images/Chat Folder.png"> <span><b>Web</b><i>Chat</i></span>
+		</div>  
+		<div id="menu">
+			<a href="login.php" id="account"><h2>Authorization</h2></a>
+		</div>
+	</div>
+	<div id="home">	
+     </div>   
+</body>
+</html>
